892a3948928d6f8b17cf42a41382903485b17357
jauharfz <jauharf14@gmail.com>
2025-03-08 10:46:54 +0700

    Add DataGeneric Class

--- 05_Generics/Program.cs
+++ 05_Generics/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("first commit 05_Generics from main branch");
+﻿public class DataGeneric
+{
+    private readonly string data;
+    public DataGeneric(string _data)
+    {
+        data = _data;
+    }
+    public void PrintData()
+    {
+        Console.WriteLine($"Daya yang disimpan adalah: {data}");
+        Console.ReadKey();
+    }
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string userInput = Console.ReadLine();
+        DataGeneric testing = new DataGeneric(userInput);
+        testing.PrintData();
+    }
+}
