c472df13350f74a65f85ef4ea77bd874c6c8fd51
jauharfz <jauharf14@gmail.com>
2025-03-11 13:59:17 +0700

    add an Addition class w/ method SumOfThreeNumbers

--- 05_JM_Generics/Program.cs
+++ 05_JM_Generics/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("test");
+﻿class Add
+{
+    public float SumOfThreeNumbers(float x, float y, float z)
+    {
+        return x + y + z;
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Add add = new Add();
+        var results = add.SumOfThreeNumbers(23, 11, 10);
+        Console.WriteLine(results);
+    }
+}
