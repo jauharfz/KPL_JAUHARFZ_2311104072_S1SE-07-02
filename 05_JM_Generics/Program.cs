eec9cb64fea262eadd6f5900e17113f892b234de
jauharfz <jauharf14@gmail.com>
2025-03-14 17:25:46 +0700

    nothing changes(ril)

--- 05_JM_Generics/Program.cs
+++ 05_JM_Generics/Program.cs
@@ -4,29 +4,32 @@ class SimpleDatabse<T>
 {
     private List<T> _storedData;
     private List<DateTime> _inputDates = new List<DateTime>();
-    
+
     public SimpleDatabse()
     {
         _storedData = new List<T>();
     }
-    
+
     public void AddNewData(T x)
     {
         _storedData.Add(x);
         _inputDates.Add(DateTime.Now);
     }
+
     public void PrintAllData()
     {
         for (var i = 0; i < _storedData.Count; i++)
         {
-            Console.WriteLine($"Data {i+1} berisi: {_storedData[i]} yang disimpan pada waktu {_inputDates[i]}");
+            Console.WriteLine($"Data {i + 1} berisi: {_storedData[i]} yang disimpan pada waktu {_inputDates[i]}");
         }
     }
-    public void SumOfThreeNumbers<T>(T x, T y, T z) where T: IAdditionOperators<T,T,T>
+
+    public void SumOfThreeNumbers<T>(T x, T y, T z) where T : IAdditionOperators<T, T, T>
     {
-        Console.WriteLine($"results penjumlahan dari {x}, {y}, {z} adalah {x+y+z}");
+        Console.WriteLine($"results penjumlahan dari {x}, {y}, {z} adalah {x + y + z}");
     }
 }
+
 class Program
 {
     static void Main()
@@ -36,6 +39,6 @@ class Program
         db.AddNewData(11);
         db.AddNewData(10);
         db.PrintAllData();
-        db.SumOfThreeNumbers(23,11,10);
+        db.SumOfThreeNumbers(23, 11, 10);
     }
 }
