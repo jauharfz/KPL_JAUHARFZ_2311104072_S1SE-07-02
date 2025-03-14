d79cab228d795db052563c0ca3b453beb0b648c0
jauharfz <jauharf14@gmail.com>
2025-03-14 13:29:49 +0700

    fix(?) :skull

--- 03_JM_GUI_Builder_dan_GitHub/Form1.cs
+++ 03_JM_GUI_Builder_dan_GitHub/Form1.cs
@@ -3,42 +3,53 @@ namespace _03_GUI_Builder_dan_GitHub;
 public partial class Form1 : Form
 {
     private double angkaPertama = 0;
-    private string operatorOperasi = " ";
+    private string currentInput = "";
     private double angkaKedua = 0;
+
     public Form1()
     {
         InitializeComponent();
         resultBox.ReadOnly = true;
         resultBox.TextAlign = HorizontalAlignment.Right;
     }
+
     private void AngkaButton_Click(object sender, EventArgs e)
     {
         Button button = (Button)sender;
+        currentInput += button.Text;
         resultBox.Text += button.Text;
     }
-
     private void Add_Click(object sender, EventArgs e)
     {
-        angkaPertama = double.Parse(resultBox.Text);
-        operatorOperasi = "+";
-        angkaKedua = double.Parse(resultBox.Text);
-        resultBox.Text = angkaPertama + operatorOperasi; //display for second number
+        if (!string.IsNullOrEmpty(currentInput))
+        {
+            angkaPertama = double.Parse(currentInput);
+            currentInput = "";
+            resultBox.Text += "+";
+        }
     }
 
     private void Results_Click(object sender, EventArgs e)
     {
-       
         double hasil = 0;
+        if (!string.IsNullOrEmpty(currentInput))
+        {
+            angkaKedua = double.Parse(currentInput);
+            hasil = angkaPertama + angkaKedua;
+        }
+        resultBox.Text = hasil.ToString();
+        angkaPertama = hasil;
 
-        switch (operatorOperasi)
+
+        /*switch (operatorOperasi)
         {
             case"+":
                 hasil = angkaPertama + angkaKedua;
                 break;
             default:
                 break;
-        }
-        resultBox.Text = hasil.ToString();
+        }*/
+        /*resultBox.Text = hasil.ToString();*/
         /*angkaPertama = hasil;
         operatorOperasi = "";*/
     }
