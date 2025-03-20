25003853a820c57425502398680232eb2cf83c42
jauharfz <jauharf14@gmail.com>
2025-03-20 23:52:08 +0700

    adding class YoutubeVideo

---
+++ 06_Design_by_Contract_and_Defensive_Programming/TP/Program.cs
@@ -0,0 +1,33 @@
+﻿class YoutubeVideo
+{
+    private readonly int id;
+    private readonly string title;
+    public int playCount { get; set; }
+
+    public YoutubeVideo(string title)
+    {
+        Random random = new Random();
+        id = random.Next(10000, 100000);
+        this.title = title;
+        playCount = 0;
+    }
+
+    public void IncreasePlayCount(int time)
+    {
+        playCount += time;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine($"ID: {id}, TITLE: {title}, PLAY_COUNT: {playCount}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var _youtubeVideo = new YoutubeVideo("Tutorial Design By Contract - jauharfz");
+        _youtubeVideo.PrintVideoDetails();
+    }
+}
