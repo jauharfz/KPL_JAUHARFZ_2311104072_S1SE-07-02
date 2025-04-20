8d06c6dcff889a2c3e2e5840f01315fbb353abc3
jauharfz <jauharf14@gmail.com>
2025-04-20 19:05:20 +0700

    TP8 done

---
+++ 08_Runtime_Configuration_and_Internalization/TP/CovidConfig.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace tpmodul8_2311104072;
+
+public class CovidConfig
+{
+    [JsonPropertyName("satuan_suhu")] public string SatuanSuhu { get; set; } = "celcius";
+    [JsonPropertyName("batas_hari_demam")] public string BatasHariDemam { get; set; } = "14";
+
+    [JsonPropertyName("pesan_ditolak")]
+    public string PesanDitolak { get; set; } = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+
+    [JsonPropertyName("pesan_diterima")]
+    public string PesanDiterima { get; set; } = "Anda dipersilahkan untuk masuk kedalam gedung ini";
+
+    public static CovidConfig ReadJSON(string filePath = "covid_config.json")
+    {
+        CovidConfig config = new CovidConfig();
+        try
+        {
+            string jsonString = File.ReadAllText(filePath);
+            var configFromJson = JsonSerializer.Deserialize<CovidConfig>(jsonString);
+            if (configFromJson != null)
+            {
+                if (configFromJson.SatuanSuhu != "CONFIG1") config.SatuanSuhu = configFromJson.SatuanSuhu;
+                if (configFromJson.BatasHariDemam != "CONFIG2") config.BatasHariDemam = configFromJson.BatasHariDemam;
+                if (configFromJson.PesanDitolak != "CONFIG3") config.PesanDitolak = configFromJson.PesanDitolak;
+                if (configFromJson.PesanDiterima != "CONFIG4") config.PesanDiterima = configFromJson.PesanDiterima;
+            }
+        }
+        catch
+        {
+
+        }
+        return config;
+
+
+    }
+
+    public void UbahSatuan()
+    {
+        if (SatuanSuhu == "celcius")
+        {
+            SatuanSuhu = "fahrenheit";
+        }
+        else
+        {
+            SatuanSuhu = "celcius";
+        }
+    }
+
+
+}
---
+++ 08_Runtime_Configuration_and_Internalization/TP/Program.cs
@@ -0,0 +1,41 @@
+﻿using tpmodul8_2311104072;
+
+var covidConfig = CovidConfig.ReadJSON("covid_config.json");
+covidConfig.UbahSatuan();
+Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {covidConfig.SatuanSuhu}");
+var userInput = Console.ReadLine();
+float.TryParse(userInput, out float result);
+Console.WriteLine("Berapa hari yang lalu(perkiraan) anda terakhir memiliki gejala demam");
+var userInput2 = Console.ReadLine();
+int.TryParse(userInput2, out int result2);
+int.TryParse(covidConfig.BatasHariDemam, out int batasHariDemam);
+
+if (result2 > batasHariDemam)
+{
+    Console.WriteLine(covidConfig.PesanDitolak);
+    return;
+}
+
+if (covidConfig.SatuanSuhu.ToLower() == "celcius")
+{
+    if (result < 36.5 || result > 37.5)
+    {
+        Console.WriteLine(covidConfig.PesanDitolak);
+        return;
+    }
+
+    Console.WriteLine(covidConfig.PesanDiterima);
+}
+else if (covidConfig.SatuanSuhu.ToLower() == "fahrenheit")
+{
+    if (result < 97.7 || result > 99.5)
+    {
+        Console.WriteLine(covidConfig.PesanDitolak);
+        return;
+    }
+
+    Console.WriteLine(covidConfig.PesanDiterima);
+}
+
+
+Console.ReadKey();
